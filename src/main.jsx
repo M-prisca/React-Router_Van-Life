9235a29e4f4155e11a3c37bd200ae52577fc6ccf
Prisca Masereli <priscamasereli@gmail.com>
2025-08-11 16:03:26 +0200

    Feat: Route, path,& element

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App() {
+  return <h1>Hello World</h1>;
+}
+
+function About() {
+  return <h1>About page goes here! 🎉</h1>;
+}
+
+/**
+ * Challenge:
+ * 1. Create an "About" component (just render another h1 that says
+ *    "About page" or something more interesting of your choosing).
+ * 2. Create a new Route to  render the About component when the path
+ *    is /about
+ */
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>
+);
