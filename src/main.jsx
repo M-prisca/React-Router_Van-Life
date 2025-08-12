3ebf8658b8bfcaae0f34455956306271db7f2b5d
Prisca Masereli <priscamasereli@gmail.com>
2025-08-12 15:23:44 +0200

    Feat(component-nested router): fixing the navbar with a Layout Rout part 2

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans";
 import VanDetail from "./pages/VanDetail.jsx";
+import Layout from "./components/Layout";
+
 import "./index.css";
 import "./server.js";
 
@@ -12,10 +14,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
