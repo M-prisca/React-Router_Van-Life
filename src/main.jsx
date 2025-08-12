4dbc37afe1ce381eec7e171daa6a8d6e55ab1285
Prisca Masereli <priscamasereli@gmail.com>
2025-08-12 11:04:35 +0200

    Feat: Add miragejs dependancies

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import "./index.css";
+import "./server.js";
 
 function App() {
   return (
