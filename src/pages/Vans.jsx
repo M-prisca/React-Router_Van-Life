3197d14038f1a2f005c68e79be1a61ad08eb1806
Prisca Masereli <priscamasereli@gmail.com>
2025-08-12 11:40:58 +0200

    Feat: Add Vans list

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 const Vans = () => {
-  return <div></div>;
+  const [vans, setVans] = React.useState([]);
+  React.useEffect(() => {
+    fetch("/api/vans")
+      .then((res) => res.json())
+      .then((data) => setVans(data.vans));
+  }, []);
+
+  const vanElements = vans.map((van) => (
+    <div key={van.id}>
+      <img src={van.imageUrl} />
+      <div>
+        <h3>{van.name}</h3>
+        <p>
+          ${van.price}
+          <span>/ day</span>
+        </p>
+      </div>
+      <i className={`van-type ${van.type} selected`}>{van.type}</i>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Explore our vans options</h1>
+      <div>{vanElements}</div>
+    </div>
+  );
 };
 
 export default Vans;
