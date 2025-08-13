3102c154a3c48b8fcea40a97e5255b6395bce751
Prisca Masereli <priscamasereli@gmail.com>
2025-08-13 11:25:56 +0200

    Feat: Active Link Styling with NavLink

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -2,16 +2,29 @@ import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
 
 const HostLayout = () => {
+  const getLinkClass = () =>
+    "text-black hover:text-red-500 hover:font-bold hover:underline transition-colors";
+
   return (
     <>
-      <nav className="bg-white flex gap-4 px-4 ">
-        <NavLink className="hover:underline" to="/host">
+      <nav className="bg-white flex gap-4 px-4 py-2">
+        <NavLink
+          to="/host"
+          end={true}
+          className={({ isActive }) => (isActive ? getLinkClass() : null)}
+        >
           Dashboard
         </NavLink>
-        <NavLink className="hover:underline" to="/host/income">
+        <NavLink
+          to="/host/income"
+          className={({ isActive }) => (isActive ? getLinkClass() : null)}
+        >
           Income
         </NavLink>
-        <NavLink className="hover:underline" to="/host/reviews">
+        <NavLink
+          to="/host/reviews"
+          className={({ isActive }) => (isActive ? getLinkClass() : null)}
+        >
           Reviews
         </NavLink>
       </nav>
