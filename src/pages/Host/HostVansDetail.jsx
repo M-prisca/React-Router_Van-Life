dda1e3b28f70e8f57bf653ce24ba5cc4d0351f9a
Prisca Masereli <priscamasereli@gmail.com>
2025-08-14 10:20:40 +0200

    Feat: Add the link back to all vans

--- src/pages/Host/HostVansDetail.jsx
+++ src/pages/Host/HostVansDetail.jsx
@@ -1,38 +1,40 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 export default function HostVansDetail() {
+  const { id } = useParams();
+  const [currentVan, setCurrentVan] = React.useState(null);
 
-    const { id } = useParams()
-    const [currentVan, setCurrentVan] = React.useState(null)
+  React.useEffect(() => {
+    fetch(`/api/host/vans/${id}`)
+      .then((res) => res.json())
+      .then((data) => setCurrentVan(data.vans));
+  }, []);
 
-    React.useEffect(() => {
-        fetch(`/api/host/vans/${id}`)
-            .then(res => res.json())
-            .then(data => setCurrentVan(data.vans))
-    }, [])
+  if (!currentVan) {
+    return <h1>Loading...</h1>;
+  }
 
-    if (!currentVan) {
-        return <h1>Loading...</h1>
-    }
+  return (
+    <section>
+      <Link to=".." relative="path" className="">
+        &larr; <span>Back to all vans</span>
+      </Link>
 
-    return (
-       <section>
-            <div className="bg-gray-100  mx-70">
-                <div className="flex justify-items-end gap-4">
-                    <img className="h-40 w-80 object-cover ml" src={currentVan.imageUrl} />
-                    <div className="host-van-detail-info-text">
-                        <i
-                            className={`van-type van-type-${currentVan.type}`}
-                        >
-                            {currentVan.type}
-                        </i>
-                        <h3>{currentVan.name}</h3>
-                        <h4>${currentVan.price}/day</h4>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
+      <div className="bg-gray-100 rounded-2xl mx-80">
+        <div className="flex justify-items-end gap-4">
+          <img
+            className="h-70 rounded-2xl w-70 object-cover m-8"
+            src={currentVan.imageUrl}
+          />
+          <div className="">
+            <i className={`bg-${currentVan.type}`}>{currentVan.type}</i>
+            <h3>{currentVan.name}</h3>
+            <h4>${currentVan.price}/day</h4>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
