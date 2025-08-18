416e0f1a50e8c006ea61f09ca7358ea21e8fed64
Prisca Masereli <priscamasereli@gmail.com>
2025-08-18 11:33:23 +0200

    Feat: setup search Params

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useSearchParams } from "react-router-dom";
 
 const Vans = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [vans, setVans] = React.useState([]);
 
+  const typeFilter = searchParams.get("type");
+
+  console.log(typeFilter);
   React.useEffect(() => {
     fetch("/api/vans")
       .then((res) => res.json())
